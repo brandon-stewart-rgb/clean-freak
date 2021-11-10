103e0e5f00b3d965994a3b2328b0a465b0819b6d
brandon-stewart-rgb <brandon@pixeltree.us>
2021-11-10 09:56:09 -0700

    feature/nav

--- client/src/components/DefaultNavbar.js
+++ client/src/components/DefaultNavbar.js
@@ -14,125 +14,133 @@ import Icon from '@material-tailwind/react/Icon';
 import Button from '@material-tailwind/react/Button';
 
 export default function DefaultNavbar() {
-    const [openNavbar, setOpenNavbar] = useState(false);
+	const [openNavbar, setOpenNavbar] = useState(false);
 
-    return (
-        <Navbar color="transparent" navbar>
-            <NavbarContainer>
-                <NavbarWrapper>
-                    <a
-                        href="/"
-                        target="_self"
-                        rel="noreferrer"
-                    >
-                        <NavbarBrand>CLEAN FREAK</NavbarBrand>
-                    </a>
-                    <NavbarToggler
-                        onClick={() => setOpenNavbar(!openNavbar)}
-                        color="white"
-                    />
-                </NavbarWrapper>
+	return (
+		<Navbar color="transparent" navbar>
+			<NavbarContainer>
+				<NavbarWrapper>
+					<a href="/" target="_self" rel="noreferrer">
+						<NavbarBrand>CLEAN FREAK</NavbarBrand>
+					</a>
+					<NavbarToggler
+						onClick={() => setOpenNavbar(!openNavbar)}
+						color="white"
+					/>
+				</NavbarWrapper>
 
-                <NavbarCollapse open={openNavbar}>
-                    <Nav>
-                        <div className="flex flex-col z-50 lg:flex-row lg:items-center">
-                            <NavLink
-                                href="https://material-tailwind.com/documentation/quick-start?ref=mtk"
-                                target="_blank"
-                                rel="noreferrer"
-                                ripple="light"
-                            >
-                                <Icon name="description" size="2xl" />
-                                &nbsp;Docs
-                            </NavLink>
-                            <NavLink
-                                href="https://material-tailwind.com/components?ref=mtk"
-                                target="_blank"
-                                rel="noreferrer"
-                                ripple="light"
-                            >
-                                <Icon name="apps" size="2xl" />
-                                &nbsp;Components
-                            </NavLink>
-                            <div className="text-white">
-                                <Dropdown
-                                    color="transparent"
-                                    size="sm"
-                                    buttonType="link"
-                                    buttonText={
-                                        <div className="py-2.5 font-medium flex items-center">
-                                            <Icon
-                                                name="view_carousel"
-                                                size="2xl"
-                                                color="white"
-                                            />
-                                            <span className="ml-2">
-                                                Templates
-                                            </span>
-                                        </div>
-                                    }
-                                    ripple="light"
-                                >
-                                    <Link to="/">
-                                        <DropdownItem color="lightBlue">
-                                            Landing
-                                        </DropdownItem>
-                                    </Link>
-                                    <Link to="/profile">
-                                        <DropdownItem color="lightBlue">
-                                            Profile
-                                        </DropdownItem>
-                                    </Link>
-                                    <Link to="/login">
-                                        <DropdownItem color="lightBlue">
-                                            Login
-                                        </DropdownItem>
-                                    </Link>
-                                    <Link to="/register">
-                                        <DropdownItem color="lightBlue">
-                                            Register
-                                        </DropdownItem>
-                                    </Link>
-                                </Dropdown>
-                            </div>
-                            <NavLink
-                                href="https://github.com/creativetimofficial/material-tailwind?ref=mtk"
-                                target="_blank"
-                                rel="noreferrer"
-                                ripple="light"
-                            >
-                                <Icon
-                                    family="font-awesome"
-                                    name="fab fa-github"
-                                    size="xl"
-                                />
-                                &nbsp;Github
-                            </NavLink>
-                            <NavLink
-                                href="https://github.com/creativetimofficial/material-tailwind/issues?ref=mtk"
-                                target="_blank"
-                                rel="noreferrer"
-                                ripple="light"
-                            >
-                                Issues
-                            </NavLink>
-                            <a
-                                href="https://www.creative-tim.com/product/material-tailwind-kit-react"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <Button
-                                    color="transparent"
-                                    className="bg-white text-black ml-4"
-                                    ripple="dark"
-                                >
-                                    Free Download
-                                </Button>
-                            </a>
-                        </div>
-                    </Nav>
-                </NavbarCollapse>
-            </NavbarContainer>
-        </Navbar>
-    );
+				<NavbarCollapse open={openNavbar}>
+					<Nav>
+						<div className="flex flex-col z-50 lg:flex-row lg:items-center">
+							<NavLink>
+								<Link
+									to="/"
+									target="_self"
+									rel="noreferrer"
+									ripple="light"
+								>
+									home
+								</Link>
+							</NavLink>
+							<NavLink>
+								<Link
+									to="/login"
+									target="_self"
+									rel="noreferrer"
+									ripple="light"
+								>
+								login
+								</Link>
+							</NavLink>
+							<NavLink>
+								<Link
+									to="/register"
+									target="_self"
+									rel="noreferrer"
+									ripple="light"
+								>
+								register
+								</Link>
+							</NavLink>
+							<NavLink>
+								<Link
+									to="/profile"
+									target="_self"
+									rel="noreferrer"
+									ripple="light"
+								>
+								profile
+								</Link>
+							</NavLink>
+							<NavLink>
+								<Link
+									to="/dashboard"
+									target="_self"
+									rel="noreferrer"
+									ripple="light"
+								>
+								dashboard
+								</Link>
+							</NavLink>
+
+
+
+						
+
+							{/* <div className="text-white">
+								<Dropdown
+									color="transparent"
+									size="sm"
+									buttonType="link"
+									buttonText={
+										<div className="py-2.5 font-medium flex items-center">
+											<Icon name="view_carousel" size="2xl" color="white" />
+											<span className="ml-2">Templates</span>
+										</div>
+									}
+									ripple="light"
+								>
+									<Link to="/">
+										<DropdownItem color="lightBlue">Landing</DropdownItem>
+									</Link>
+									<Link to="/profile">
+										<DropdownItem color="lightBlue">Profile</DropdownItem>
+									</Link>
+									<Link to="/login">
+										<DropdownItem color="lightBlue">Login</DropdownItem>
+									</Link>
+									<Link to="/register">
+										<DropdownItem color="lightBlue">Register</DropdownItem>
+									</Link>
+								</Dropdown>
+							</div> */}
+							<NavLink
+								href="https://github.com/brandon-stewart-rgb/clean-freak"
+								target="_blank"
+								rel="noreferrer"
+								ripple="light"
+							>
+								<Icon family="font-awesome" name="fab fa-github" size="xl" />
+								&nbsp;Github
+							</NavLink>
+							
+							<a
+								href="https://www.creative-tim.com/product/material-tailwind-kit-react"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<Button
+									color="transparent"
+									className="bg-white text-black ml-4"
+									ripple="dark"
+								>
+									Free Download
+								</Button>
+							</a>
+						</div>
+					</Nav>
+				</NavbarCollapse>
+			</NavbarContainer>
+		</Navbar>
+	);
 }
