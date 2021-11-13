f884d121b9c41e2746892bf0897f2a0d4ce3fa39
Noah <noahm0217@gmail.com>
2021-11-13 11:48:08 -0700

    mutaton commented out

--- server/schemas/resolvers/order.js
+++ server/schemas/resolvers/order.js
@@ -29,53 +29,54 @@ const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 
 //////UPDATE MUTATION//////
-const GET_TODOS = gql`
-  query GetTodos {
-    todos {
-      id
-    }
-  }
-`;
+// const GET_TODOS = gql`
+//   query GetTodos {
+//     todos {
+//       id
+//     }
+//   }
+// `;
 
-function AddTodo() {
-    let input;
-    const [addTodo] = useMutation(ADD_TODO, {
-      update(cache, { data: { addTodo } }) {
-        cache.modify({
-            fields: {
-              todos(existingTodos = []) {
-                const newTodoRef = cache.writeFragment({
-                  data: addTodo,
-                  fragment: gql`
-                    fragment NewTodo on Todo {
-                      id
-                      type
-                    }
-                  `
-                });
-                return [...existingTodos, newTodoRef];
-              }
-            }
-          });
-        }
-      });
+// function AddTodo() {
+//     let input;
+//     const [addTodo] = useMutation(ADD_TODO, {
+//       update(cache, { data: { addTodo } }) {
+//         cache.modify({
+//             fields: {
+//               todos(existingTodos = []) {
+//                 const newTodoRef = cache.writeFragment({
+//                   data: addTodo,
+//                   fragment: gql`
+//                     fragment NewTodo on Todo {
+//                       id
+//                       type
+//                     }
+//                   `
+//                 });
+//                 return [...existingTodos, newTodoRef];
+//               }
+//             }
+//           });
+//         }
+//       });
 
-      return (
-        <div>
-          <form
-            onSubmit={e => {
-              e.preventDefault();
-              addTodo({ variables: { type: input.value } });
-              input.value = "";
-            }}
-          >
-              <input
-          ref={node => {
-            input = node;
-          }}
-        />
-        <button type="submit">Add Todo</button>
-      </form>
-    </div>
-  );
-}
+//       return (
+//         <div>
+//           <form
+//             onSubmit={e => {
+//               e.preventDefault();
+//               addTodo({ variables: { type: input.value } });
+//               input.value = "";
+//             }}
+//           >
+//               <input
+//           ref={node => {
+//             input = node;
+//           }}
+//         />
+//         <button type="submit">Add Todo</button>
+//       </form>
+//     </div>
+//   );
+// }
+//////UPDATE MUTATION//////
