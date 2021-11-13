8aa3f9ad0203499b4bbb4b27a491cfca1f94f2cd
Noah <noahm0217@gmail.com>
2021-11-12 18:41:29 -0700

    2nd half of items/packages

--- server/schemas/resolvers/order.js
+++ server/schemas/resolvers/order.js
@@ -11,3 +11,6 @@ const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 // const storeItems = new Map([
 //     [1, { priceInCents: 5000, name: "Cleaning package 1" }],
 //     [2, { priceInCents: 10000, name: "Cleaning package 2" }],
+//     [3, { priceInCents: 15000, name: "Cleaning package 3" }],
+//     [4, { priceInCents: 20000, name: "Cleaning package 4" }],
+// ])
