06523888c6f4b4a90aee3fa896ac68b7c3c03790
Noah <noahm0217@gmail.com>
2021-11-12 18:39:19 -0700

    stripe auth const/app.use

--- server/schemas/resolvers/order.js
+++ server/schemas/resolvers/order.js
@@ -1,6 +1,12 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { Order } = require('../../models');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+//const express = require("express")
+//const app = express()
+//app.use(express.json())
+//app.use(express.static("public"))
+//require("dotenv").config()
+
 
 const resolvers = {
   
