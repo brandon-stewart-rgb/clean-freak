420c50094f37df8dd70da4ad6ea309d1c1e93bf4
Noah <noahm0217@gmail.com>
2021-11-13 11:46:14 -0700

    update mutation complete needs testing

--- server/schemas/resolvers/order.js
+++ server/schemas/resolvers/order.js
@@ -59,3 +59,23 @@ function AddTodo() {
           });
         }
       });
+
+      return (
+        <div>
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              addTodo({ variables: { type: input.value } });
+              input.value = "";
+            }}
+          >
+              <input
+          ref={node => {
+            input = node;
+          }}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
+}
