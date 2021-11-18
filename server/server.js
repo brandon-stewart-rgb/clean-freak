2bf8f399f58a7f85805989de380a476e42734c82
brandon-stewart-rgb <brandon@pixeltree.us>
2021-11-18 08:39:22 -0700

    package.json server.js update

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 db.once('open', () => {
